94a926eb09c793b2916004d5cd057e4801c1554f
olegmir59 <udom1@ya.ru>
2024-09-04 23:07:45 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+# @   print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# Задача  1  Арифметика
+print("1st program")
+print(9**0.5 * 5)
+
+# Задача  2  Логика
+print("2st program")
+print( 9.99 > 9.98 and 1000 != 1000.1 )
+
+# Задача  3  Школьная загадка
+print("3st program")
+print(2*2 + 2 == 2*(2 + 2))
+
+# Задача  4  Первый символ после точки
+print("4st program")
+s = '123.456'
+print(s[s.find(".")+1])
+
+print(int(float(s)*10) % 10)  # алгоритм по заданию
